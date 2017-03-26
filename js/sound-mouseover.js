d2a747ffc3be1abe0ad108a0dfe8276dda5b4711
Shawn Montgomery <shawnkmontgomery@gmail.com>
2017-03-26 05:38:16 -0400

    added play and pause functions for real time audio

---
+++ js/sound-mouseover.js
@@ -0,0 +1,10 @@
+
+function playclip() {
+	var audio = document.getElementsByTagName("video")[0];
+	audio.play();
+}
+
+function pauseclip() {
+	var audio = document.getElementsByTagName("video")[0];
+	audio.pause();
+}
