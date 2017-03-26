624171da095a67360f8b76098a1cc71485a2d00c
Shawn Montgomery <shawnkmontgomery@gmail.com>
2017-03-26 07:26:07 -0400

    More changes to start game correctly

--- js/sound-mouseover.js
+++ js/sound-mouseover.js
@@ -2,7 +2,11 @@
 function playclip() {
 	var audio = document.getElementsByTagName("video")[0];
 	audio.play();
-	start();
+}
+
+function startGame() {
+	var obj = document.getElementsByTagName("object")[0];
+	obj.start();
 }
 
 function pauseclip() {
