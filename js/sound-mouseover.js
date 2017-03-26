fa3a3bd3956f4d056455de44ce028f189e1ae0d4
Shawn Montgomery <shawnkmontgomery@gmail.com>
2017-03-26 07:45:04 -0400

    deleted changes

--- js/sound-mouseover.js
+++ js/sound-mouseover.js
@@ -4,16 +4,6 @@ function playclip() {
 	audio.play();
 }
 
-function startGame() {
-	var obj = document.getElementsByTagName("g");
-	for (var i = 0; i < obj.length; i++) {
-		var str = obj[i].id();
-		if (str == "Var_Text") {
-			obj.start();
-		}
-	}
-}
-
 function pauseclip() {
 	var audio = document.getElementsByTagName("video")[0];
 	audio.pause();
