a765d0570e3319647d2e930bbf37983c5a13ebc6
Shawn Montgomery <shawnkmontgomery@gmail.com>
2017-03-26 07:40:28 -0400

    even more

--- js/sound-mouseover.js
+++ js/sound-mouseover.js
@@ -5,8 +5,13 @@ function playclip() {
 }
 
 function startGame() {
-	var obj = document.getElementById("Var_Text");
-	obj.start();
+	var obj = document.getElementsByTagName("g");
+	for (var i = 0; i < obj.length; i++) {
+		var str = obj[i].id();
+		if (str == "Var_Text") {
+			obj.start();
+		}
+	}
 }
 
 function pauseclip() {
